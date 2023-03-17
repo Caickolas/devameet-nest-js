1376a8ba069379f573e7f8e7bb8db3b26af36ec1
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2023-03-17 17:11:00 -0300

    sign in, sign up e jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JWTStrategy } from "./strategies/jwt.strategy";
 
 @Module({
-    imports:[],
+    imports:[
+        UserModule,
+        JwtModule.register({
+            secret: process.env.USER_JWT_SECRET_KEY
+        })
+    ],
     controllers:[AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JWTStrategy]
 })
 export class AuthModule {}
