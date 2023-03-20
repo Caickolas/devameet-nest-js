79e107f702bc22797c9ffd111aaee1107ef65728
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2023-03-20 17:22:34 -0300

    finalizando gerenciamentos de reunioes e atualização de salas com objetos

--- src/meet/meet.controller.ts
+++ src/meet/meet.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Request, Post, Body, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Request, Post, Body, Delete, Param, Put } from '@nestjs/common';
 import { CreateMeetDto } from './dtos/createmeet.dto';
 import { GetMeetDto } from './dtos/getmeet.dto';
+import { UpdateMeetDto } from './dtos/updatemeet.dto';
 import { MeetService } from './meet.service';
 
 @Controller('meet')
@@ -36,5 +37,21 @@ export class MeetController {
         const { id } = params;
         
         await this.service.deleteMeetsByUser(userId, id)
+    }
+
+    @Get('objects/:id')
+    async getObjectsByMeetId(@Request() req, @Param() params ){
+        const { userId } = req?.user;
+        const { id } = params;
+        
+        return await this.service.getMeetObjects( id ,userId)
+    }
+
+    @Put(':id')
+    async UpdateMeet(@Request() req, @Param() params, @Body() dto: UpdateMeetDto ){
+        const { userId } = req?.user;
+        const { id } = params;
+        
+        await this.service.update(id, userId, dto)
     }
 }
--- src/meet/meet.service.ts
+++ src/meet/meet.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserService } from 'src/user/user.service';
 import { CreateMeetDto } from './dtos/createmeet.dto';
 import { GetMeetDto } from './dtos/getmeet.dto';
+import { UpdateMeetDto } from './dtos/updatemeet.dto';
 import { generateLink } from './helpers/linkgenerator.helper';
+import { MeetMessagesHelper } from './helpers/meetmessages.helper';
 import { Meet, MeetDocument } from './schemas/meet.schema';
+import { MeetObject, MeetObjectDocument } from './schemas/meetobject.schema';
 
 @Injectable()
 export class MeetService {
@@ -13,7 +16,8 @@ export class MeetService {
 
     constructor(
         @InjectModel(Meet.name) private readonly model: Model<MeetDocument>,
-        private readonly userservice: UserService
+        @InjectModel(MeetObject.name) private readonly objectModel: Model<MeetObjectDocument>,
+        private readonly userservice: UserService,
     ){}
 
     async getMeetsByUser(userId: String){
@@ -41,5 +45,40 @@ export class MeetService {
         return await this.model.deleteOne({user: userId, _id: meetId});
     }
 
-    
+    async getMeetObjects(meetId: string, userId: string){
+        this.logger.debug(`getMeetObjects - ${userId} - ${meetId} `);
+
+        const user = await this.userservice.getUserById(userId);
+        const meet = await this.model.findOne({user, _id: meetId});
+
+        return await this.objectModel.find({meet});
+    }
+
+    async update(meetId: string, userId: string, dto: UpdateMeetDto){
+        this.logger.debug(`update - ${userId} - ${meetId} `);
+
+        const user = await this.userservice.getUserById(userId);
+        const meet = await this.model.findOne({user, _id: meetId});
+
+        if(!meet){
+            throw new BadRequestException(MeetMessagesHelper.UPDATE_MEET_NOT_FOUND);
+        }
+
+        meet.name = dto.name;
+        meet.color = dto.color;
+        await this.model.findByIdAndUpdate({_id: meetId}, meet);
+
+        await this.objectModel.deleteMany({meet});
+
+        let objectPayload;
+
+        for (const object of dto.objects) {
+            objectPayload = {
+                meet,
+                ...object
+            }
+
+            await this.objectModel.create(objectPayload);''
+        }
+    }
 }
